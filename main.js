e63a1c8dfb6082cd8ba0049f0188846ecb583443
James Halliday <mail@substack.net>
2014-03-14 13:01:18 -0500

    move slider into scope

--- main.js
+++ main.js
@@ -1,48 +1,20 @@
+var baudio = require('webaudio');
+var observable = require('observable');
+
 var scope = require('./lib/scope.js')();
 scope.appendTo('#scope');
+
 window.addEventListener('resize', function (ev) { scope.resize() });
-
-var baudio = require('webaudio');
-var slideways = require('slideways');
-
-var slider = slideways({
-    min: 0.001,
-    max: 3,
-    init: Math.log(50) / Math.log(10)
-});
-slider.appendTo('#slider');
-
-var rate = 44000;
-var duration = 1 / 50;
-
-slider.on('value', function (x) {
-    scope.setDuration(Math.pow(10, -x));
-});
 
 var music = function (t) { return 0 };
 var code = document.querySelector('#code');
-code.addEventListener('keydown', onchange);
-code.addEventListener('change', onchange);
-code.addEventListener('keyup', onchange);
-onchange();
 
-var lastErr, lastSrc;
-function onchange () {
-    try { music = Function(code.value)() }
-    catch (err) {
-        if (String(err) !== lastErr) console.log(err);
-        lastErr = String(err);
-        return;
-    }
-    if (lastSrc !== code.value) scope.wave(music, 500);
-    lastSrc = code.value;
-}
-
-var floats = new Float32Array(44000 / 8);
-var floatIx = 0;
-var b = baudio(function (t) {
-    var x = music(t);
-    floats[floatIx % floats.length] = x;
-    return x;
+observable.input(code)(function (source) {
+    try { music = Function(source)() }
+    catch (err) { return console.log(err) }
+    
+    scope.wave(music, 500);
 });
+
+var b = baudio(function (t) { return music(t) });
 b.play();
