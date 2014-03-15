fa5e106bbc6f96e333d2d73f61941095f96d93e6
James Halliday <mail@substack.net>
2014-03-15 03:38:27 -0300

    frequency viewer too

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
 var baudio = require('webaudio');
 var observable = require('observable');
 
-var scope = require('./lib/scope.js')();
-scope.appendTo('#scope');
+var music = function (t) { return 0 };
+var ascope = require('amplitude-viewer')(function (t) { return music(t) });
+ascope.appendTo('#ascope');
+
+var fscope = require('frequency-viewer')();
+fscope.on('click', function () { fscope.draw(data) });
+fscope.appendTo('#fscope');
 
 var paused = false;
-scope.on('click', function () { paused = !paused });
+ascope.on('click', function () { paused = !paused });
 
-window.addEventListener('resize', function (ev) { scope.resize() });
+window.addEventListener('resize', function (ev) {
+    ascope.resize();
+    fscope.resize();
+});
 
-var music = function (t) { return 0 };
 var code = document.querySelector('#code');
 
 observable.input(code)(function (source) {
     try { music = Function(source)() }
     catch (err) { return console.log(err) }
-    scope.wave(music, 500);
+    ascope.draw();
 });
 
-var time = 0;
-setInterval(function () {
-    if (!paused) scope.setTime(time);
+setInterval(function f () {
+    if (paused) return;
+    ascope.setTime(time);
+    ascope.draw();
 }, 50);
+
+setInterval(function () {
+    if (!paused) fscope.draw(data);
+}, 50);
+
+var time = 0;
+var data = new Float32Array(4000);
+var dataIx = 0;
 
 var b = baudio(function (t) {
     time = t;
     if (paused) return 0;
-    return music(t);
+    var x = music(t);
+    data[dataIx++ % data.length] = x;
+    return x;
 });
 b.play();
