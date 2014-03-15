9094d556e5577d6f70b14aad751635e82fa488f0
James Halliday <mail@substack.net>
2014-03-15 04:50:13 -0300

    fix the server

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 var http = require('http');
-var server = http.createServer(ecstatic);
 var ecstatic = require('ecstatic')(__dirname + '/..');
+var server = http.createServer(ecstatic);
 server.listen(parseInt(process.argv[2]) || 0);
 
 server.on('listening', function () {
-    console.log('listening on :' + server.address().port);
+    console.log('listening on http://localhost:' + server.address().port);
 });
